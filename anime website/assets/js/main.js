f0dba7749940dad64d34ede718dab8ca19c81c71
Gregory <elvismabisi@gmail.com>
2023-12-07 10:40:50 +0300

    completed the recommend for you part and the special anime section all together including the styling

--- anime website/assets/js/main.js
+++ anime website/assets/js/main.js
@@ -9,27 +9,50 @@
 /*=============== SCROLL HEADER ===============*/
 
 /*=============== BRAND SWIPER ===============*/
-var brandSwiper = new Swiper('.brand-swiper',{
+var brandSwiper = new Swiper('.brand-swiper', {
     spaceBetween: 24,
     loop: true,
     breakpoints: {
         450: {
-            slidesPerview: 2,
+            slidesPerView: 2,
             spaceBetween: 24,
         },
         768: {
-            slidesPerview: 3,
+            slidesPerView: 3,
             spaceBetween: 24,
         },
         992: {
-            slidesPerview: 4,
+            slidesPerView: 4,
             spaceBetween: 24,
         },
         1200: {
-            slidesPerview:5,
-            spaceBetween:24,
+            slidesPerView: 5,
+            spaceBetween: 24,
         },
     },
 });
 
+
 /*=============== ANIME SWIPER ===============*/
+var animeSwiper = new Swiper('.anime-swiper', {
+    spaceBetween: 24,
+    loop: true,
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+        1400: {
+            slidesPerView: 5,
+            spaceBetween: 24,
+        },
+    },
+});
