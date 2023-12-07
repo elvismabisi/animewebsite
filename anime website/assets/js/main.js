d0f6f8708e71152b0b4dff2fa1e4fb78be45496f
Gregory <elvismabisi@gmail.com>
2023-12-07 09:56:45 +0300

    completed work on the header and brands section have started on the special anime part

--- anime website/assets/js/main.js
+++ anime website/assets/js/main.js
@@ -9,5 +9,27 @@
 /*=============== SCROLL HEADER ===============*/
 
 /*=============== BRAND SWIPER ===============*/
+var brandSwiper = new Swiper('.brand-swiper',{
+    spaceBetween: 24,
+    loop: true,
+    breakpoints: {
+        450: {
+            slidesPerview: 2,
+            spaceBetween: 24,
+        },
+        768: {
+            slidesPerview: 3,
+            spaceBetween: 24,
+        },
+        992: {
+            slidesPerview: 4,
+            spaceBetween: 24,
+        },
+        1200: {
+            slidesPerview:5,
+            spaceBetween:24,
+        },
+    },
+});
 
 /*=============== ANIME SWIPER ===============*/
